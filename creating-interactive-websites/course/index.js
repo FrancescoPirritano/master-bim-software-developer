0f3164ec70652c0cd03d3a7b8c60240396dc8b94
francesco.pirritano <francesco.pirritano@gmail.com>
2024-03-12 13:58:34 +0000

    Introduce If/Else statement

--- creating-interactive-websites/course/index.js
+++ creating-interactive-websites/course/index.js
@@ -1,17 +1,20 @@
 //One of the 3 ways to create function with the syntax function"name of the function"() {}
-function showModal() {
-    const modal = document.getElementById("new-project-modal")
-    modal.showModal()  
+function showModal(id) {
+    const modal = document.getElementById(id)
+    if(modal) {
+        modal.showModal()
+    } else {
+        console.warn("Modal ID was not found:", id)
+    }
+      
 }
-
-/*
-One of the 3 ways to create function: syntax const"name of the function" = () => {}
-const showModal = () => {
-    const modal = document.getElementById("new-project-modal")
-    modal.showModal()
-}
-*/
 
 //This document object is provided by the browser, and its main purpose is to help us interact.
-const newProjectBtn = document.getElementById("new-projects-btn")
-newProjectBtn.addEventListener("click", showModal) //addEventListener is a method used for an event to happen, e.g. when the user click.
+const newProjectBtn = document.getElementById("new-project-btn")
+if(newProjectBtn) { //runs if true
+    newProjectBtn.addEventListener("click", () => {showModal("new-project-modal")}) 
+    //addEventListener is a method used for an event to happen, e.g. when the user click.
+} else { //runs if false
+    console.warn("New project button was not found:")
+}
+
