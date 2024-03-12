251449deef24e3871f922c837b3703b31ed027f4
francesco.pirritano <francesco.pirritano@gmail.com>
2024-03-12 08:41:08 +0000

    Update HTML

--- creating-interactive-websites/course/index.js
+++ creating-interactive-websites/course/index.js
@@ -1,2 +1,17 @@
+//One of the 3 ways to create function with the syntax function"name of the function"() {}
+function showModal() {
+    const modal = document.getElementById("new-project-modal")
+    modal.showModal()  
+}
+
+/*
+One of the 3 ways to create function: syntax const"name of the function" = () => {}
+const showModal = () => {
+    const modal = document.getElementById("new-project-modal")
+    modal.showModal()
+}
+*/
+
 //This document object is provided by the browser, and its main purpose is to help us interact.
 const newProjectBtn = document.getElementById("new-projects-btn")
+newProjectBtn.addEventListener("click", showModal) //addEventListener is a method used for an event to happen, e.g. when the user click.
