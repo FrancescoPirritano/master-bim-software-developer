0b01fa005ec7ff643dc3944a03ab9ba16c1adc54
francesco.pirritano <francesco.pirritano@gmail.com>
2024-03-20 21:06:45 +0000

    ProjectsManager and toggleModal

---
+++ creating-interactive-websites/course/src/class/ProjectsManager.ts
@@ -0,0 +1,26 @@
+import { IProject, Project } from "./Project"
+
+//Master Class existing for the whole application to keep reference to all projects,
+// be able to delete them, import/export them, etc.
+export class ProjectsManager {
+    //List property to keep a reference to all projects. All the projects will be instances of the project Class created in
+    // Project.ts.
+    //We import Project.ts and define the type of this List as project. We use [] to tell Typescript that it's a list of 
+    //project instances.
+    list: Project[] = []
+    ui: HTMLElement //Container for all the project cards
+
+    //Diplay the project card in the web
+    constructor(container: HTMLElement) {
+        this.ui = container
+    }
+
+    //Create a method for the new project.
+    newProject(data: IProject) {
+        const project = new Project(data)
+        this.ui.append(project.ui) //This inserts a dumb element inside of another, e.g. 
+        this.list.push(project)
+        return project //return project is the return value of the newProject method
+    }
+
+}
--- creating-interactive-websites/course/src/index.ts
+++ creating-interactive-websites/course/src/index.ts
@@ -1,6 +1,10 @@
 import { Project, IProject, ProjectStatus, UserRole } from "./class/Project"
+// Project class is not used anymore once we introduce the ProjectsManager class.
+import { ProjectsManager } from "./class/ProjectsManager" //Import ProjectsManager to create a new List and append data
 
+/*
 //One of the 3 ways to create function with the syntax function"name of the function"() {}
+//Open Modal 
 function showModal(id: string) {
     const modal = document.getElementById(id)
     if(modal && modal instanceof HTMLDialogElement) { // instanceof is to ensure modal is an HTML element 
@@ -9,12 +13,48 @@ function showModal(id: string) {
         console.warn("Modal ID was not found:", id)
     }
       
+} 
+
+//Close Modal
+function closeModal(id: string) {
+    const modal = document.getElementById(id)
+    if(modal && modal instanceof HTMLDialogElement) { // instanceof is to ensure modal is an HTML element 
+        modal.close()
+    } else {
+        console.warn("Modal ID was not found:", id)
+    }
+      
+} */
+
+//Define accept/cancel properties
+const submitNewProjectButton = document.getElementById("submit-new-project-btn") as HTMLElement
+const cancelNewProjectButton = document.getElementById("cancel-new-project-btn") as HTMLElement
+
+//Toggle Modal
+function toggleModal(id: string) {
+    const modal = document.getElementById(id)
+    if(modal && modal instanceof HTMLDialogElement) {
+        if(modal.open) {
+            modal.close()
+        } else {
+            modal.showModal()
+        }
+    } else {
+        console.warn("Modal ID was not found:", id)
+    }
 }
+
+//Before the ProjectsManager class runs, get a reference to the project list in the HTML document
+const projectsListUI = document.getElementById("projects-list") as HTMLElement
+
+//Create an instance of the ProjectsManager 
+const projectsManager = new ProjectsManager(projectsListUI)
+
 
 //This document object is provided by the browser, and its main purpose is to help us interact.
 const newProjectBtn = document.getElementById("new-project-btn")
 if(newProjectBtn) { //runs if true
-    newProjectBtn.addEventListener("click", () => {showModal("new-project-modal")}) 
+    newProjectBtn.addEventListener("click", () => {toggleModal("new-project-modal")}) 
     //addEventListener is a method used for an event to happen, e.g. when the user click.
 } else { //runs if false
     console.warn("New project button was not found:")
@@ -33,7 +73,9 @@ if(projectForm && projectForm instanceof HTMLFormElement) { // instanceof is to 
             status: projectFormData.get("projectStatus") as ProjectStatus,
             finishDate: new Date(projectFormData.get("projectFinishDate") as string),
         }
-        const project = new Project(projectData)
+        const project = projectsManager.newProject(projectData)
+        projectForm.reset() //Reset the form once submitted.
+        toggleModal("new-project-modal") //Close the modal after it's submitted.
     })
 } else {
     console.warn("The project form was not found. Check the ID")
